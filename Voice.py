89c1c4784eb3f0f2ef2fdb2fe4f7dc353762fce6
tybanks12 <77938231+tybanks12@users.noreply.github.com>
2022-04-22 11:44:48 -0400

    Add files via upload

---
+++ Voice.py
@@ -0,0 +1,42 @@
+import speech_recognition as sr
+import pyttsx3
+
+def speak(text):
+    engine = pyttsx3.init()
+    engine.say(text)
+    engine.runAndWait()
+
+
+def main():
+    r = sr.Recognizer()
+
+    with sr.Microphone() as source:
+        r.adjust_for_ambient_noise(source)
+
+        print("Please say something")
+
+        audio = r.listen(source)
+
+        # print("Recognizing Now .... ")
+
+        # recognize speech using google
+
+        try:
+            x = r.recognize_google(audio)
+            print("You have said \n" + x)
+            if x == "hello Fred":
+                print("good anakin, gooood")
+            print("Audio Recorded Successfully \n ")
+
+
+        except Exception as e:
+            print("Error :  " + str(e))
+
+        # write audio
+
+        with open("recorded.wav", "wb") as f:
+            f.write(audio.get_wav_data())
+
+
+if __name__ == "__main__":
+    main()
